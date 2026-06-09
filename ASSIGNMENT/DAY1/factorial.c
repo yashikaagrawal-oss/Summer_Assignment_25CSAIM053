9412ff578e3dd332efc8708302155c3a8eebc859
yashikaagrawal-oss <yashika.agrawal2025@glbajajgroup.org>
2026-06-09 12:03:01 +0530

    Add files via upload

---
+++ ASSIGNMENT/DAY1/factorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{int n;
+long long fact=1;
+printf("enter a number:");
+scanf("%d", &n);
+ for (int i =1;i<=n; i++)
+{fact*=i;
+}
+printf("Factorial = %lld", fact);
+return 0;
+}
