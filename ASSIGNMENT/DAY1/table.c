bd570037af3c15ee9b70175126c7bc36b2a93006
yashikaagrawal-oss <yashika.agrawal2025@glbajajgroup.org>
2026-06-09 12:08:56 +0530

    Delete ASSIGNMENT/DAY1 directory

--- ASSIGNMENT/DAY1/table.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
- int main() 
- { int n; 
-printf("enter a number:");
- scanf("%d", &n);
-   for (int i=1;i<=10;i++)
-    {     printf("%d x %d = %d\n",n,i,n*i);} 
-     return 0 ;}
